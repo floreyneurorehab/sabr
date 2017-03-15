13ac43fecb80891808122b678b59b9c24aa75431
Peter Goodin <peter.goodin@florey.edu.au>
2017-03-15 15:05:00 +1100

    Working prototype of de-identification and convertion to BIDS.

--- de_id.py
+++ de_id.py
@@ -1,5 +1,8 @@
+
 import dicom
 import pandas as pd
+import re
+import fnmatch
 import os
 
 '''
@@ -8,22 +11,22 @@ Peter Goodin, Dec 2016
 
 
 #Get spreadsheet type from final 3 strings of main_sheet.
-def sabr_subj_ss_check(subj_sheet):
+def sabr_subj_ss_check(subj_ss):
     subj_ss_type = subj_ss[-3:]
 
     if subj_ss_type == 'csv':
         try:
             try:
-                subj_id_df = pd.read_csv(subj_ss, sep = ',', names = ['sub_name', 'sub_id'])
+                subj_df = pd.read_csv(subj_ss, sep = ',', names = ['sub_name', 'sub_id'])
             except:
-                subj_id_df = pd.read_csv(subj_ss, sep = '\t', names = ['sub_name', 'sub_id'])
+                subj_df = pd.read_csv(subj_ss, sep = '\t', names = ['sub_name', 'sub_id'])
         except:
             print '\nDelimiter not recoginised. Please ensure your spreadsheet uses either commas (,) or tabbed spaces to separate cells.\n'
             exit()
 
     elif subj_ss_type == 'xls':
         try:
-            subj_id_df = pd.read_excel(subj_ss)
+            subj_df = pd.read_excel(subj_ss)
         except:
             print '\nUnable to read excel sheet. Please ensure data is formatted properly.\n'
             exit()
@@ -31,7 +34,7 @@ def sabr_subj_ss_check(subj_sheet):
     else:
         print 'Spreadsheet type not recognised. Please use either comma or tabbed separated or excel formats.'
         exit()
-    return subj_id_df
+    return subj_df
     
 
 def sabr_scan_ss_check(scan_ss):
@@ -41,16 +44,16 @@ def sabr_scan_ss_check(scan_ss):
     if scan_ss_type == 'csv':
         try:
             try:
-                scan_path_df = pd.read_csv(scan_ss, sep = ',', names = ['scan_path', 'scan_type'])
+                scan_df = pd.read_csv(scan_ss, sep = ',', names = ['scan_match', 'scan_type', 'scan_filename'])
             except:
-                scan_path_df = pd.read_csv(scan_ss, sep = '\t', names = ['scan_path', 'scan_type'])
+                scan_df = pd.read_csv(scan_ss, sep = '\t', names = ['scan_match', 'scan_type', 'scan_filename'])
         except:
             print '\nDelimiter not recoginised. Please ensure your spreadsheet uses either commas (,) or tabbed spaces to separate cells.\n'
             exit()
 
     elif scan_ss_type == 'xls':
         try:
-            scan_path_df = pd.read_excel(scan_ss)
+            scan_df = pd.read_excel(scan_ss)
         except:
             print '\nUnable to read excel sheet. Please ensure data is formatted properly.\n'
             exit()
@@ -59,137 +62,133 @@ def sabr_scan_ss_check(scan_ss):
         print 'Spreadsheet type not recognised. Please use either comma or tabbed separated or excel formats.'
         exit()
 
-    return scan_path_df
+    return scan_df
 
 
 
+def sabr_deid(subj_df, scan_df, raw_dir, deid_outdir, sub_name):
 
+    subj_info = subj_df[subj_df["sub_name"] == sub_name]
 
-def sabr_deid(subj_id_df, scan_path_df):
+    #Join raw dir with subject name (assumes directory structure is ./rawdir/subj_name/...
+    subj_main_dir = os.path.join(raw_dir, subj_info.sub_name[0])
 
-try:
-    os.mkdir(deid_outdir)
-except:
-    print('\n***De-identified folder exists. Not creating new folder.***\n')
+    new_id = subj_info.sub_id[0]
 
+    #Get list of sessions within main subj directory, make dir and loop over sessions.
+    subj_sessions = os.walk(subj_main_dir).next()[1]
+    subj_sessions.sort()
 
-#Loop through subjects in list
-    for n in range(0, len(subj_id_df)):
-        #Get subject ID from dataframe
-        new_id = subj_id_df.sub_id[n]
-        
-        #Create deidentified main (root) directory
-        deid_main_folder = os.path.join(deid_outdir, new_id)
-        try:
-            os.mkdir(deid_main_folder)
-        except:
-            print '\nFolder exists\n'
+    #Create deidentified main (root) directory for subject
+    subj_deid_main_dir = os.path.join(deid_outdir, new_id)        
+    try:
+        os.mkdir(subj_deid_main_dir)
+    except:
+        print '\nDirectory exists\n'
        
-        #Join raw dir with subject name (assumes directory structure is ./rawdir/subj_name/...
-        subj_main_folder = os.path.join(rawdir, subj_id_df.sub_name[n])
+    for sn, session in enumerate(subj_sessions):
+    #MAKE DIRECTORIES BUT ZERO PAD SESSION
+        subj_deid_session_dir = os.path.join(subj_deid_main_dir, 'run-' '{:02d}'.format(sn+1))
 
-        #Get list of sessions within main subj directory, make dir and loop over sessions.
-        subj_sessions = os.walk(subj_main_folder).next()[1]
+        try: 
+            os.mkdir(subj_deid_session_dir)
+        except:
+            print '\nSession folder exists\n'
 
-        for sn, session in enumerate(subj_sessions):
-        #MAKE DIRECTORIES BUT ZERO PAD SESSION
-            deid_session_folder = os.path.join(deid_main_folder, 'session_' '{:03d}'.format(sn+1))
+        #Session folder for identifiable subject
+        subj_session_dir = os.path.join(subj_main_dir, session)
 
-            try: 
-                os.mkdir(deid_session_folder)
-            except:
-                print '\nSession folder exists\n'
-
-            #Session folder for identifiable subject
-            subj_session_folder = os.path.join(subj_main_folder, session)
-
-            #Loop over scan folder types within scan dataframe (anat, epi etc) 
-            for j in range(0,len(scan_path_df)):
+        #Loop over scan folder types within scan dataframe (anat, epi etc) 
+        for j, scan in enumerate(scan_df['scan_type']):
                 
-                #Match common sequence substring with path in os.walk
-                for root, dr, files in os.walk(subj_session_folder):
-                    match = scan_path_df.scan_path[j]
+            #Match common sequence substring with path in os.walk
+            for root, dr, files in os.walk(subj_session_dir):                
+                match = scan_df.scan_match[j] 
+
+                match_regex = fnmatch.translate(match)
+                found = re.search(match_regex, root)
+
                 
-                    #If match, start deid process, not not, move onto next folder.
-                    if match in root:
-                        deid_sequence_folder = os.path.join(deid_session_folder, scan_path_df.scan_type[j])
-                        try:
-                            os.mkdir(deid_sequence_folder)
-                            print('Making directory' + deid_sequence_folder)
-                        except:
-                            print('\nSequence folder exists\n')
+                #If match, start deid process, not not, move onto next folder.
+                if found != None:
+                    #print(j, match, root)
+                    subj_deid_sequence_dir = os.path.join(subj_deid_session_dir, scan)
+                    try:
+                        os.mkdir(subj_deid_sequence_dir)
+                        print('Making directory' + subj_deid_sequence_dir)
+                    except:
+                        print('\nSequence directory ' + scan + ' exists\n')
                         
-                        #Create list of dicoms in sequence dir rather than use 
-                        #files (more control in case any non-dicoms)
-                        anon_files = os.listdir(root)
-                        anon_files = [x for x in anon_files if 'nii' not in x] #Remove any previous nii files that may be present < To do - expand to other file types (mgh, analyze, etc)
-                        anon_files.sort()
+                    #Create list of dicoms in sequence dir rather than use 
+                    #files (more control in case any non-dicoms)
+                    anon_files = os.listdir(root)
+                    anon_files = [x for x in anon_files if 'nii' not in x] #Remove any previous nii files that may be present < To do - expand to other file types (mgh, analyze, etc)
+                    anon_files.sort()
                 
-                        for anon_file in anon_files: 
-                            #Read files in 1 at a time, remove the remove / alter the below tags.
-                            dcm = dicom.read_file(os.path.join(root, anon_file), force = True) #Uses force = True incase dicoms haven't had identifier added to header
+                    for anon_file in anon_files: 
+                        #Read files in 1 at a time, remove the remove / alter the below tags.
+                        dcm = dicom.read_file(os.path.join(root, anon_file), force = True) #Uses force = True incase dicoms haven't had identifier added to header                           
                             
-                            #dcm.remove_private_tags()
+                        #Strip aquisition date information
+                        aqusition_date_list = [[0x0008,0x0020],[0x0008,0x0021],[0x0008,0x0022],[0x0008,0x0023]]
                             
-                            #Strip aquisition date information
-                            aqusition_date_list = [[0x0008,0x0020],[0x0008,0x0021],[0x0008,0x0022],[0x0008,0x0023]]
-                            
-                            for tag in aqusition_date_list:
-                                try:
-                                    dcm[hex(tag[0]), hex(tag[1])].value = ''
-                                except:
-                                    print ''
-                                #   print 'Tag %s %s does not exist in dicom. Moving to next tag' % (hex(tag[0]), hex(tag[1]))
+                        for tag in aqusition_date_list:
+                            try:
+                                dcm[hex(tag[0]), hex(tag[1])].value = ''
+                            except:
+                                print ''
+                                #print 'Tag %s %s does not exist in dicom. Moving to next tag' % (hex(tag[0]), hex(tag[1]))
                             
                     
-                            #Strip aquisition time information
-                            aqusition_time_list = [[0x0008,0x0030],[0x0008,0x0031],[0x0008,0x0032],[0x0008,0x0033]]
+                        #Strip aquisition time information
+                        aqusition_time_list = [[0x0008,0x0030],[0x0008,0x0031],[0x0008,0x0032],[0x0008,0x0033]]
                             
-                            for tag in aqusition_time_list:
-                                try:
-                                    dcm[hex(tag[0]), hex(tag[1])].value = ''
-                                except:
-                                    print ''
-                                #    print 'Tag %s %s does not exist in dicom. Moving to next tag' % (hex(tag[0]), hex(tag[1]))
+                        for tag in aqusition_time_list:
+                            try:
+                                dcm[hex(tag[0]), hex(tag[1])].value = ''
+                            except:
+                                print ''
+                                #print 'Tag %s %s does not exist in dicom. Moving to next tag' % (hex(tag[0]), hex(tag[1]))
                             
                     
-                            #Strip physician information
-                            physician_list = [[0x0008,0x0090],[0x0008,0x1050]]
+                        #Strip physician information
+                        physician_list = [[0x0008,0x0090],[0x0008,0x1050]]
                             
-                            for tag in physician_list:
-                                try:
-                                    dcm[hex(tag[0]), hex(tag[1])].value = ''
-                                except:
-                                    print ''
-                                #    print 'Tag %s %s does not exist in dicom. Moving to next tag' % (hex(tag[0]), hex(tag[1]))
+                        for tag in physician_list:
+                            try:
+                                dcm[hex(tag[0]), hex(tag[1])].value = ''
+                            except:
+                                print ''
+                                #print 'Tag %s %s does not exist in dicom. Moving to next tag' % (hex(tag[0]), hex(tag[1]))
                             
                             
                             #Strip study description
                             #dcm[0x0008,0x1030].value = ''
                     
                             #Strip subject name / patient ID
-                            subj_name_list = [[0x0010,0x0010],[0x0010,0x0020]]
+                        subj_name_list = [[0x0010,0x0010],[0x0010,0x0020]]
                             #PatientName, PatientID
                             
-                            for tag in subj_name_list:
-                                try:
-                                    dcm[hex(tag[0]), hex(tag[1])].value = new_id
-                                except:
-                                    print ''
-                                #    print 'Tag %s %s does not exist in dicom. Moving to next tag' % (hex(tag[0]), hex(tag[1]))
+                        for tag in subj_name_list:
+                            try:
+                                dcm[hex(tag[0]), hex(tag[1])].value = new_id
+                            except:
+                                print ''
+                                #print 'Tag %s %s does not exist in dicom. Moving to next tag' % (hex(tag[0]), hex(tag[1]))
                             
                             
                             #Strip subject attributes
-                            subj_attrib_list = [[0x0010,0x0030],[0x0010,0x1010],[0x0010,0x1020],[0x0010,0x1030]]
-                            #, DoB, Age, PatientHeight, PatientWeight                
+                        subj_attrib_list = [[0x0010,0x0030],[0x0010,0x1010],[0x0010,0x1020],[0x0010,0x1030]]
+                        #, DoB, Age, PatientHeight, PatientWeight                
             
-                            for tag in subj_attrib_list:
-                                try:
-                                    dcm[hex(tag[0]), hex(tag[1])].value = ''
-                                except:
-                                    print ''
-                                    #print 'Tag %s %s does not exist in dicom. Moving to next tag' % (hex(tag[0]), hex(tag[1]))
+                        for tag in subj_attrib_list:
+                            try:
+                                dcm[hex(tag[0]), hex(tag[1])].value = ''
+                            except:
+                                print ''
+                                #print 'Tag %s %s does not exist in dicom. Moving to next tag' % (hex(tag[0]), hex(tag[1]))
                             
                             
-                            #Write anonymised file
-                            dicom.write_file(os.path.join(deid_sequence_folder, anon_file),dcm)
+                        #Write anonymised file
+                        dicom.write_file(os.path.join(subj_deid_sequence_dir, anon_file),dcm)
+    return(subj_deid_main_dir)
--- run.py
+++ run.py
@@ -7,7 +7,7 @@ Created on Thu Dec  1 10:14:50 2016
 """
 
 from de_id import sabr_subj_ss_check, sabr_scan_ss_check, sabr_deid
-from nii_convert import sabr_dcm2niix_check
+from nii_convert import sabr_dcm2niix_check, sabr_dcm2niix_convert
 import os
 
 
@@ -19,26 +19,40 @@ raw_dir = '/home/peter/Desktop/deid_script/siemens'
 deid_outdir = h_dir + 'de_id/'
 nii_outdir = h_dir + 'nii/'
 
-
 #Option to keep de-id' dicoms
 keep_deid = 'y'
 
 #Location of subject and scan lists
-subj_ss = raw_dir + 'sub_list.csv'
-scan_ss = raw_dir + 'scan_list.csv'
+subj_ss = h_dir + 'sub_list.csv'
+scan_ss = h_dir + 'scan_list.csv'
 
-#Check input sheets
-subj_sheet = sabr_subj_ss_check(subj_ss)
-scan_sheet = sabr_scan_ss_check(scan_ss)
+#Check input sheets, convert to dataframe
+subj_df = sabr_subj_ss_check(subj_ss)
+scan_df = sabr_scan_ss_check(scan_ss)
 
-#Run de-id
-sabr_deid(subj_sheet, scan_sheet)
-
-#Check dcm2niix install
+#Check dcm2niix is installed
 sabr_dcm2niix_check()
 
-#Run conversion
-sabr_dcm2niix_convert()
+#Make directory to store deidentified data
+try:
+    os.mkdir(deid_outdir)
+except:
+    print('\n***De-identified folder exists. Not creating new folder.***\n')
+
+#Run de-id
+#Loop through subjects in dataframe
+for n in range(0, len(subj_df)):
+    sub_name = subj_df.sub_name[n]
+    subj_deid_main_dir = sabr_deid(subj_df, scan_df, raw_dir, deid_outdir, sub_name)
+    
+
+    #Run conversion
+    new_id = os.path.split(subj_deid_main_dir)[1]
+    #print('The new ID is {}'.format(new_id))
+    subj_nii_outdir = os.path.join(nii_outdir, new_id)
+    sabr_dcm2niix_convert(subj_deid_main_dir, subj_nii_outdir, new_id, scan_df)
+    
+print('\nConversion complete.')
 
 
 
